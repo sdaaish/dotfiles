22744c808f4b3a74cdb74c2882ca2b01fecd47ca
Stig Dahl <stig@charlottendal.net>
2016-08-08 20:50:22 +0200

    Added visual fingerprinting too

--- bin/ssh-check.sh
+++ bin/ssh-check.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Example to generate SHA256+Base64 fingerprints from public key
@@ -7,5 +8,7 @@
 for file in /etc/ssh/ssh_*.pub;do
     check=$(awk '{print $2}' ${file} |base64 -d|sha256sum -b|sed 's/ .*$//'|xxd -r -p|base64)
     printf "${file} has SHA256 checksum ${check}\n"
+    printf "And has visual fingerprint:\n"
+    ssh-keygen -lvf ${file}
 done
 
