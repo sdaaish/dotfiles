de20b50644e6d58cfc45154d4da9d69b9831c56c
Stig Dahl <stig@charlottendal.net>
2016-08-08 20:00:23 +0200

    Added ssh-auths and ssh-check.

---
+++ bin/ssh-check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Example to generate SHA256+Base64 fingerprints from public key
+## From https://www.lastbreach.com/en/blog/ssh-public-key-verification-with-fingerprinthash
+#
+
+for file in /etc/ssh/ssh_*.pub;do
+    check=$(awk '{print $2}' ${file} |base64 -d|sha256sum -b|sed 's/ .*$//'|xxd -r -p|base64)
+    printf "${file} has SHA256 checksum ${check}\n"
+done
+
