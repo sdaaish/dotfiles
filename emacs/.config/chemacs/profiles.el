515e3fcd33e4e7b2ea528c883e68dfe27f101418
Stig Dahl <stig@charlottendal.net>
2022-09-14 23:50:19 +0200

    Update submodules

--- emacs/.config/chemacs/profiles.el
+++ emacs/.config/chemacs/profiles.el
@@ -1,3 +1,4 @@
 (("default" . ((user-emacs-directory . "~/.config/emacs.default")))
+ ("work" . ((user-emacs-directory . "~/.config/emacs.work")))
  ("doom" . ((user-emacs-directory . "~/.config/emacs.doom")
             (env . (("DOOMDIR" . "~/.config/doom"))))))
