7763dc39df1f236d10e399057726d4c66a6d2893
Stig Dahl <stig@charlottendal.net>
2023-01-18 23:59:31 +0100

    Add dir-locals

---
+++ home/.dir-locals.el
@@ -0,0 +1,4 @@
+((auto-mode-alist . (("\\.toml\\.tmpl\\'" . toml-mode)))
+ (auto-mode-alist . (("\\.chezmoi.*" . toml-mode)))
+ (auto-mode-alist . (("\\.ps1\\.tmpl\\'" . powershell-mode)))
+ (auto-mode-alist . (("\\.sh\\.tmpl\\'" . sh-mode))))
