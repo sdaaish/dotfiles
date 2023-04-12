6211a4fec6372ceb9ff2a6d586e18b8ddb350793
Stig Dahl <stig@charlottendal.net>
2023-04-12 23:25:26 +0200

    fix: Remove older files

--- emacs/.config/chemacs/profiles.el
+++
@@ -1,4 +0,0 @@
-(("default" . ((user-emacs-directory . "~/.config/emacs.default")))
- ("work" . ((user-emacs-directory . "~/.config/emacs.work")))
- ("doom" . ((user-emacs-directory . "~/.config/emacs.doom")
-            (env . (("DOOMDIR" . "~/.config/doom"))))))
