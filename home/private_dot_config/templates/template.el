0b37caa32705bcca448cfe16990fb7ff13dde764
Stig Dahl <stig@charlottendal.net>
2023-08-17 01:00:51 +0200

    Add templates for Emacs

---
+++ home/private_dot_config/templates/template.el
@@ -0,0 +1,24 @@
+;;; `(upcase (file-name-nondirectory (file-name-sans-extension (buffer-file-name))))` --- Summary
+;;
+;; Author: `(concat user-full-name)` <`(concat user-mail-address)`>
+;; Created: `(format-time-string "%e %B %Y")`
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;  $1
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+$0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; `(file-name-nondirectory (buffer-file-name))` ends here
