4d54ee3a519cef2deb5178d0d52c747c1db66daa
Stig Dahl <stig@charlottendal.net>
2023-10-24 21:09:21 +0200

    Adjust paths to work in linux too

--- home/private_dot_config/powershell/MyFunctions/MyFunctions/Public/Test-Admin.ps1
+++ home/private_dot_config/powershell/MyFunctions/MyFunctions/Public/Test-Admin.ps1
@@ -1,7 +1,9 @@
 function Test-Admin
 {
-    $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $prp = New-Object System.Security.Principal.WindowsPrincipal($wid)
-    $adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
-    $prp.IsInRole($adm)
+    if (-not($isLinux)){
+        $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+        $prp = New-Object System.Security.Principal.WindowsPrincipal($wid)
+        $adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+        $prp.IsInRole($adm)
+    }
 }
