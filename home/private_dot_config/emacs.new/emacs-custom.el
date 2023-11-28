c1a48a626391ef4a0c4c7769c7f76bfa7e108938
Stig Dahl <stig@charlottendal.net>
2023-11-28 22:13:25 +0100

    fix: Corrected font selection

--- home/private_dot_config/emacs.new/emacs-custom.el
+++ home/private_dot_config/emacs.new/emacs-custom.el
@@ -11,6 +11,6 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "CaskaydiaCove Nerd Font Mono" :foundry "outline" :slant normal :weight regular :height 140 :width normal))))
  '(fixed-pitch ((t (:height 140 :family "CaskaydiaCove Nerd Font Mono"))))
- '(fixed-pitch-serif ((t (:height 140 :family "CaskaydiaCove Nerd Font Code"))))
+ '(fixed-pitch-serif ((t (:height 140 :family "CaskaydiaCove Nerd Font Mono"))))
  '(mode-line-active ((t (:inherit mode-line :foreground "khaki"))))
  '(variable-pitch ((t (:height 140 :foundry "outline" :family "Comic Sans MS")))))
