4744f1e4d1ab81a7828f80a0caab12df72a766c6
Stig Dahl <stig@charlottendal.net>
2024-04-16 23:14:25 +0200

    Update quotes

--- home/private_dot_config/lisp/code.el
+++ home/private_dot_config/lisp/code.el
@@ -1,7 +1,31 @@
+;;; CODE --- Summary  -*- lexical-binding: t; -*-
+;;
+;; Author: Stig Dahl
+;; Created: 2024-04-16
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+
 (defun my/prog-mode ()
   "Enables useful setting for programming mode."
   (interactive)
   (smartparens-mode t)
   (rainbow-delimiters-mode t)
-  (aggressive-indent-mode t)
-  )
+  (aggressive-indent-mode t))
+
+(provide 'code)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; code.el ends here
--- home/private_dot_config/lisp/myquotes.el
+++ home/private_dot_config/lisp/myquotes.el
@@ -1,5 +1,21 @@
-;;; myquotes.el --- basic funtions -*- lexical-binding:t -*-
-
+;;; MYQUOTES --- Summary  -*- lexical-binding:t -*-
+;;
+;; Author: Stig Dahl
+;; Created: 2024-04-16
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;;; Code:
 
 (defun my/quote-word (arg)
@@ -8,5 +24,32 @@
   (forward-whitespace 1)
   (insert "\"")
   (forward-word arg)
-  (insert "\"")
-  )
+  (insert "\""))
+
+;; From Emacs Elements
+(defun replace-smart-quotes (beg end)
+  "Replace 'smart quotes' in buffer or region with ascii quotes.
+      Operates on the entire buffer if no region is selected."
+  (interactive
+   (if (use-region-p)
+	     (list (region-beginning) (region-end))
+     (list (point-min) (point-max))))
+  (format-replace-strings '(("\x201C" . "\"")
+			                      ("\x201D" . "\"")
+			                      ("\x2018" . "'")
+			                      ("\x2019" . "'"))
+			                    nil beg end))
+(global-set-key (kbd "C-c q") 'replace-smart-quotes)
+
+;; From Emacs Elements
+(defun yank-and-replace-smart-quotes ()
+  "Yank (paste) and replace smart quotes from the source with ascii quotes."
+  (interactive)
+  (yank)
+  (replace-smart-quotes (mark) (point)))
+
+;; (global-set-key (kbd "C-c y") 'yank-and-replace-smart-quotes)
+
+(provide 'myquotes)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; quoting.el ends here
