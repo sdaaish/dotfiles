32d3327c96f47662a07f0af88703f131d4a692f0
Stig Dahl <stig@charlottendal.net>
2024-09-07 01:58:06 +0200

    Add Python stuff

--- home/private_dot_config/emacs.new/elisp/init-python.el
+++ home/private_dot_config/emacs.new/elisp/init-python.el
@@ -29,6 +29,27 @@
   (highlight-indent-guides-method 'character)
   (python-indent-guess-indent-offset-verbose nil))
 
+(use-package python
+  :bind (:map python-mode-map
+              ("C-c M-j" . run-python)
+              ("C-c C-k" . python-shell-send-buffer)
+              ("C-c C-c" . python-shell-send-defun)
+              ("C-c C-p" . python-shell-send-defun)
+              ("C-x C-e" . python-shell-send-statement))
+  :custom
+  (python-indent-guess-indent-offset-verbose nil)
+  ;;  (python-check-command "poetry run pyright")
+  (python-indent-offset 4))
+
+;; Save for future use, read the pet man page
+;; (use-package python-pytest)
+;; (use-package python-black)
+;; (use-package python-isort)
+;; (use-package ruff-format)
+
+(use-package pet
+  :config
+  (add-hook 'python-base-mode-hook 'pet-mode -10))
 
 (provide 'init-python)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
