e24fef3fd2210b65be8cae0cc9c8b8caf7307e08
Stig Dahl <stig@charlottendal.net>
2025-01-28 00:11:50 +0100

    Add utils

--- home/.dir-locals.el
+++ home/.dir-locals.el
@@ -1,4 +1,8 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
 ((auto-mode-alist . (("\\.toml\\.tmpl\\'" . toml-mode)))
  (auto-mode-alist . (("\\.chezmoi.*" . toml-mode)))
  (auto-mode-alist . (("\\.ps1\\.tmpl\\'" . powershell-mode)))
- (auto-mode-alist . (("\\.sh\\.tmpl\\'" . sh-mode))))
+ (auto-mode-alist . (("\\.sh\\.tmpl\\'" . sh-mode)))
+ (powershell-mode . ((powershell-indent . 4))))
