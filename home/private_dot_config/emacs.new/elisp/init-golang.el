fa08af575a1be5e46a7f8d4c652d84c35494ebe5
Stig Dahl <stig@charlottendal.net>
2025-04-22 15:15:52 +0200

    Update go

--- home/private_dot_config/emacs.new/elisp/init-golang.el
+++ home/private_dot_config/emacs.new/elisp/init-golang.el
@@ -6,11 +6,16 @@
     (cons 'go-module root)))
 
 (use-package go-mode
-  :config (add-to-list 'major-mode-remap-alist '(go-mode . go-ts-mode)))
+  :config (add-to-list 'major-mode-remap-alist '(go-mode . go-ts-mode))
+  :hook ((go-mode go-ts-mode) . 'my/line-number-relative))
 
 (add-hook 'project-find-functions #'project-find-go-module)
 
 (cl-defmethod project-root ((project (head go-module)))
   (cdr project))
+
+;; See https://jeffbowman.writeas.com/crafted-emacs-example-adding-go
+(use-package go-eldoc
+  :hook ((go-mode go-ts-mode) . 'go-eldoc-setup))
 
 (provide 'init-golang)
