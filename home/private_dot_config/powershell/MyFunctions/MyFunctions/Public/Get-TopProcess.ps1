d6ec7abd3e7ee5fdab65678dd7a81c66e50d449d
Stig Dahl <stig@charlottendal.net>
2025-04-22 13:04:07 +0200

    Modify process info to make it more visible

--- home/private_dot_config/powershell/MyFunctions/MyFunctions/Public/Get-TopProcess.ps1
+++ home/private_dot_config/powershell/MyFunctions/MyFunctions/Public/Get-TopProcess.ps1
@@ -1,4 +1,6 @@
-# show top n processes regarding cpu usage or total memory
+# Show top n processes regarding cpu usage or total memory
+# See example from Microsoft:
+#  https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-process?view=powershell-7.5#example-5-add-a-property-to-the-default-get-process-output-display
 Function Get-TopProcess {
     param(
         [ValidateSet("Process", "Memory")]
@@ -11,10 +13,14 @@ Function Get-TopProcess {
     while ($true) {
         Clear-Host
         Get-Process |
-            Select-Object -Property Id, ProcessName,
+            Select-Object -Property @{Name="SI"; Expression = {$_.SessionId}},
+            @{Name="PID"; Expression = {$_.Id}},
             @{name = "CPU"; expression = { ($_.CPU).ToString("#") -as [int] } },
-            @{name = "TotalMemory (M)"; expression = { ($_.VM + $_.PM) / 1MB -as [int] } },
-            Handles | Sort-Object -Property $sort -Descending -Top 20 | Format-Table -Auto
+            @{name = "TotalMemory (M)"; expression = { ($_.VM + $_.PM) / 1MB -as [int] }},
+            @{Name="Handles"; Expression = {[int] $_.Handles}},
+            @{Name = "Threads"; Expression = { $_.Threads.Count}}, ProcessName |
+            Sort-Object -Property $sort -Descending -Top 20 |
+            Format-Table -Auto
         Start-Sleep 5
     }
 }
