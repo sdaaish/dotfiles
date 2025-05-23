56242cef788827e76d5ceed23a29f2a40ae37149
Stig Dahl <stig@charlottendal.net>
2025-05-24 01:28:53 +0200

    Add completions for fish

--- home/private_dot_config/fish/config.fish
+++ home/private_dot_config/fish/config.fish
@@ -1,6 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+    # Abbreviations
+    abbr -a -U -- gco 'git checkout'
+    abbr -a -U -- gts 'git status'
+    abbr -a -U -- cm 'chezmoi'
+
+    # Use zoxide
+    zoxide init fish | source
+
     # Use starship
     starship init fish | source
+
 end
