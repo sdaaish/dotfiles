d2f34d59c58753efccab1dc98b9cf9b17b054f61
Stig Dahl <stig@charlottendal.net>
2026-02-11 21:35:44 +0100

    Add a multisearch script with ripgrep and fzf

---
+++ home/bin/executable_rfe.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# From https://github.com/junegunn/fzf/blob/master/ADVANCED.md#controlling-ripgrep-search-and-fzf-search-simultaneously
+
+export TEMP=$(mktemp -u)
+trap 'rm -f "$TEMP"' EXIT
+
+INITIAL_QUERY="${*:-}"
+TRANSFORMER='
+  rg_pat={q:1}      # The first word is passed to ripgrep
+  fzf_pat={q:2..}   # The rest are passed to fzf
+
+  if ! [[ -r "$TEMP" ]] || [[ $rg_pat != $(cat "$TEMP") ]]; then
+    echo "$rg_pat" > "$TEMP"
+    printf "reload:sleep 0.1; rg --column --line-number --no-heading --color=always --smart-case %q || true" "$rg_pat"
+  fi
+  echo "+search:$fzf_pat"
+'
+fzf --ansi --disabled --query "$INITIAL_QUERY" \
+    --with-shell 'bash -c' \
+    --bind "start,change:transform:$TRANSFORMER" \
+    --color "hl:-1:underline,hl+:-1:underline:reverse" \
+    --delimiter : \
+    --preview 'bat --color=always {1} --highlight-line {2}' \
+    --preview-window 'up,60%,border-line,+{2}+3/3,~3' \
+    --bind 'enter:become($EDITOR {1} +{2})'
