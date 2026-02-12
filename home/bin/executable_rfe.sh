2f546a996e48b09d1c20d55139850af8b6a4cfb3
Stig Dahl <stig@charlottendal.net>
2026-02-12 14:03:13 +0100

    Customize rfe.sh to new defaults

--- home/bin/executable_rfe.sh
+++ home/bin/executable_rfe.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This query searches for files containing the first pattern, and then for files matching the second pattern.
+# I have excluded CSV files from being searched.
+
 # From https://github.com/junegunn/fzf/blob/master/ADVANCED.md#controlling-ripgrep-search-and-fzf-search-simultaneously
 
 export TEMP=$(mktemp -u)
@@ -12,15 +15,19 @@ TRANSFORMER='
 
   if ! [[ -r "$TEMP" ]] || [[ $rg_pat != $(cat "$TEMP") ]]; then
     echo "$rg_pat" > "$TEMP"
-    printf "reload:sleep 0.1; rg --column --line-number --no-heading --color=always --smart-case %q || true" "$rg_pat"
+    printf "reload:sleep 0.1; rg --type-not=csv --column --line-number --no-heading --color=always --smart-case %q || true" "$rg_pat"
   fi
   echo "+search:$fzf_pat"
 '
 fzf --ansi --disabled --query "$INITIAL_QUERY" \
     --with-shell 'bash -c' \
+    --header 'CTRL-/: Toggle preview | CTRL-SPC: Toggle wrap' \
     --bind "start,change:transform:$TRANSFORMER" \
+    --bind 'ctrl-/:change-preview-window(right|down|hidden)' \
+    --bind 'ctrl-space:toggle-wrap' \
+    --bind 'enter:become($EDITOR {1} +{2})' \
     --color "hl:-1:underline,hl+:-1:underline:reverse" \
     --delimiter : \
+    --height '60%' \
     --preview 'bat --color=always {1} --highlight-line {2}' \
-    --preview-window 'up,60%,border-line,+{2}+3/3,~3' \
-    --bind 'enter:become($EDITOR {1} +{2})'
+    --preview-window 'right,60%,border-line,+{2}+3/3,~3'
