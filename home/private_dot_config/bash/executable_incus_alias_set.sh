a8d7c8327eb77fce443d7426b4f525a2feb319b6
Stig Dahl <stig@charlottendal.net>
2026-07-25 13:20:06 +0200

    Update incus aliases

--- home/private_dot_config/bash/executable_incus_alias_set.sh
+++ home/private_dot_config/bash/executable_incus_alias_set.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+# Create aliases for Incus
+
+_add_alias() {
+    incus alias remove --quiet "$1" 2>/dev/null
+    incus alias add --quiet "$1" "$2" 2>/dev/null
+}
 
 # Set incus aliases
 if [ $(command -v incus) ]
 then
 
     current=$(incus alias list -f compact,noheader|awk '{print $1}')
-    incus alias add ps "list status=running -c ns4beumDS" 2>/dev/null
+    _add_alias ps "list status=running -c ns4beumDS"
+    _add_alias rl "remote list"
+    _add_alias rs "remote switch local"
+    _add_alias c "remote get-default"
+    _add_alias a "alias list"
+    _add_alias fl "list --fast"
     incus alias list
 fi
