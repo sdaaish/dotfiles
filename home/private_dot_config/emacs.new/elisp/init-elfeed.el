448833b7bed72770b1cc4e366d06328f5a2c11f0
Stig Dahl <stig@charlottendal.net>
2026-08-17 23:10:06 +0200

    Add initial elfeed config

---
+++ home/private_dot_config/emacs.new/elisp/init-elfeed.el
@@ -0,0 +1,34 @@
+;;; INIT-ELFEED --- Summary -*- lexical-binding: t; -*-
+;;
+;; Author: Stig Dahl
+;; Created: 2026-08-10
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;  Use elfeed as RSS reader
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+(use-package elfeed
+  :config
+  (set-face-attribute 'elfeed-search-unread-title-face nil :weight 'normal :foreground "khaki2"))
+
+(use-package elfeed-org
+  :after elfeed
+  :custom
+  (rmh-elfeed-org-files (list (expand-file-name "elfeed.org" my/orgdir)))
+  (rmh-elfeed-org-auto-ignore-invalid-feeds t))
+
+
+(provide 'init-elfeed)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-elfeed.el ends here
