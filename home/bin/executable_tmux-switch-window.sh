ccdd1a517a666c6d55155de7f8969f790130835d
Stig Dahl <stig@charlottendal.net>
2026-08-22 00:03:18 +0200

    Use major version for actions/checkout

--- home/bin/executable_tmux-switch-window.sh
+++ home/bin/executable_tmux-switch-window.sh
@@ -2,7 +2,7 @@
 
 # A tmux window switcher using FZF
 tmux list-panes -aF "#{p20:session_name} #{p20:window_name} - #{p-3:window_id} #{p-3:pane_id}"|\
-    #grep -vF "$(tmux display-message -p '#{windows_id}')" |\
+    #grep -vF "$(tmux display-message -p '#{windows_id}')" |
     fzf --preview="tmux list-panes -t {-2} -F '#{p20:window_name} #{=/20/...:pane_title} - windows:#{p-10:session_windows} - panes:#{p-2:window_panes}'" \
         --preview-window="up,10%,20%" \
         --reverse \
